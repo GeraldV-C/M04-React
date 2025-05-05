e30bda65853215bfe9a7fe5bbaf3ffbaf5a972de
GeraldV-C <geraldvivas.ch@gmail.com>
2025-05-04 23:01:06 -0400

    Desafio 3 Gerald V.

--- Desafio 1.3/pizzeria-mamma-mia/src/components/Navbar.jsx
+++ Desafio 1.3/pizzeria-mamma-mia/src/components/Navbar.jsx
@@ -1,28 +1,8 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import formatPrice from '../utils/formatPrice';
-
 const Navbar = () => {
-  const total = 25000;
-  const token = false;
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-      <a className="navbar-brand" href="#">🍕 Home</a>
-      <div className="ms-auto d-flex gap-2">
-        {token ? (
-          <>
-            <button className="btn btn-outline-light">🔓 Profile</button>
-            <button className="btn btn-outline-light">🔒 Logout</button>
-          </>
-        ) : (
-          <>
-            <button className="btn btn-outline-light">🔐 Login</button>
-            <button className="btn btn-outline-light">🔐 Register</button>
-          </>
-        )}
-        <button className="btn btn-success">🛒 Total: ${formatPrice(total)}</button>
-      </div>
+    <nav>
+      <h1>Pizzería Mamma Mía</h1>
+      <button>Carrito</button>
     </nav>
   );
 };
